f7d66fad3b595600ef8fdafc5d2fd7bcb1bea644
vishakha95 <vbhosale25@gmail.com>
2020-10-13 19:15:40 +1100

    new update

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
 	<title></title>
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
- 
+ <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap" rel="stylesheet">
+
+
 </head>
 <body>
 	
@@ -60,27 +64,72 @@
   </ul>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="la.jpg" alt="Los Angeles" width="1100" height="500">
+      <img src="images/1.jpg" alt="Los Angeles" width="1100" height="500">
       <div class="carousel-caption">
         <h3>Los Angeles</h3>
         <p>We had such a great time in LA!</p>
       </div>   
     </div>
     <div class="carousel-item">
-      <img src="chicago.jpg" alt="Chicago" width="1100" height="500">
+      <img src="images/2.jpg" alt="Chicago" width="1100" height="500">
       <div class="carousel-caption">
         <h3>Chicago</h3>
         <p>Thank you, Chicago!</p>
       </div>   
     </div>
     <div class="carousel-item">
-      <img src="ny.jpg" alt="New York" width="1100" height="500">
+      <img src="images/3.jpg" alt="New York" width="1100" height="500">
       <div class="carousel-caption">
         <h3>New York</h3>
         <p>We love the Big Apple!</p>
       </div>   
     </div>
   </div>
+  
+  <section class = "my-5">
+  	<div class = "py-5">
+  		<h2 class ="text-center">About Me</h2>
+  	</div>
+  	<div class = "container-fluid">
+  		<div class = "row">
+	  		<div class = "col-lg-6 col-md-6 col-12">
+	  			<img src="images\1.jpg" class ="img-fluid aboutimg">
+	  		</div>
+	  		<div class = "col-lg-6 col-md-6 col-12">
+	  			<h2 class = "dispay-4">Vishakha Bhosale</h2>
+	  			<p class = "py-4"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  			proident, sunt in culpa qui of ficia deserunt mollit anim id est laborum.Master of IT graduate from Monash University.  </p>
+	  			<a href="about.php"> Know More</a>
+	  		</div>
+	  	</div>
+ 	</div>
+  </section>
+
+  <section class = "my-5">
+  	<div class = "py-5">
+  		<h2 class ="text-center">MMMM</h2>
+  	</div>
+  	<div class = "container-fluid">
+  		<div class = "row">
+  			<div class = "col-lg-4 col-md-4 col-12">
+  				<div class="card" style="width: 400px">
+					<div class="card" style="width:400px">
+						  <img class="card-img-top" src="img_avatar1.png" alt="Card image">
+						  	<div class="card-body">
+							    <h4 class="card-title">John Doe</h4>
+							    <p class="card-text">Some example text.</p>
+							    <a href="#" class="btn btn-primary">See Profile</a>
+						 	</div>
+					</div>
+				</div>
+  			</div>
+  		</div>
+  	</div>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>	
